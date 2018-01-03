e8148ea5b4d1c29feae9152c9c91e981aefe5e1f
christianbillp <christianbillp@gmail.com>
2018-01-03 21:02:52 +0000

    Save

--- relay.py
+++ relay.py
@@ -1,5 +1,6 @@
 import pigpio
 import time
+import sys
 
 # Relays are active LOW
 
@@ -13,14 +14,26 @@ pi = pigpio.pi()
 for pin in channels:
     pi.set_mode(pin, pigpio.OUTPUT)
 
+def activate(channel):
+    pi.write(channels[channel], 0)
+
+def deactivate(channel):
+    pi.write(channels[channel], 1)
+
 # Test
-for i in range(4):
-    print('Running {}'.format(i))
-    pi.write(channels[i], 0)
-    time.sleep(1)
-    pi.write(channels[i], 1)
+#for i in range(4):
+#    print('Running {}'.format(i))
+#    pi.write(channels[i], 0)
+#    time.sleep(1)
+#    pi.write(channels[i], 1)
 
 if __name__ == '__main__':
-
+    arguments = sys.argv
+    if arguments[1] == '--help':
+        print('Usage: python3 relay.py a 2 for activating channel 2, d for deactivate')
+    elif arguments[1] == 'a':
+        activate(int(arguments[2]))
+    elif arguments[1] == 'd':
+        deactivate(int(arguments[2])) 
 
 
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pigpiod -s 2 -b 200 -f
